3ad0068b8c5a720ea1bbdea12cc080881093877a
spandanb <spandan.777@gmail.com>
2015-07-19 11:02:52 -0400

    Angularjs- getting messages through the controller. Changes to be committed: 	modified:   app.js 	modified:   index.html

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var app = angular.module('myMessenger', []);
 
 app.controller('myController',
     function($scope){
-        $scope.hello = 'Hello world!';
+        $scope.messages = [
+          {sender: 'Alice', body:'Hello, how are you?', side:'left'},
+          {sender: 'Bob', body:'I am fine, how are you?', side: 'right'},
+          {sender: 'Alice', body:'Never been better', side: 'left'}
+        ];
 });
 
 
