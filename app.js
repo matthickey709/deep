503a250a11b3339075e6e815dc652887b45c3acf
spandanb <spandan.777@gmail.com>
2015-07-19 10:48:37 -0400

    Starting angular Changes to be committed: 	modified:   app.js 	modified:   index.html

--- app.js
+++ app.js
@@ -1,34 +1,8 @@
-function add_message(){
-    var div_outer = document.createElement("div");
-    var div_inner = document.createElement("div");
-    div_inner.className = "bubble right";
-    div_outer.appendChild(div_inner);
+var app = angular.module('myMessenger', []);
 
-    var username  = document.createElement("h4");
-    var usernameText   = document.createTextNode("Bob");
-    username.appendChild(usernameText);
-    div_inner.appendChild(username);
+app.controller('myController',
+    function($scope){
+        $scope.hello = 'Hello world!';
+});
 
-    var messageText = document.getElementsByName("message")[0].value;
 
-    var message = document.createElement("div");
-    var messageNode = document.createTextNode(messageText);
-    message.appendChild(messageNode);
-    div_inner.appendChild(message);
-
-    var bubbles = document.getElementsByClassName("bubble");
-
-    var oneBubble = bubbles[0];
-
-    var parent = oneBubble.parentNode.parentNode;
-
-    var messageBox = document.getElementsByName("message")[0].parentNode.parentNode.parentNode;
-
-    //parent.appendChild(div_outer);
-    parent.insertBefore(div_outer, messageBox);
-    
-}
-
-window.onload = function(){
-    document.getElementsByTagName('button')[0].onclick = add_message;
-}
