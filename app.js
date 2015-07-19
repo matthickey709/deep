346de0a7409eee065f02be815e3f89f1ab0c8467
spandanb <spandan.777@gmail.com>
2015-07-19 13:54:00 -0400

    Angular- add new messages Changes to be committed: 	modified:   app.js 	modified:   index.html

--- app.js
+++ app.js
@@ -2,11 +2,25 @@ var app = angular.module('myMessenger', []);
 
 app.controller('myController',
     function($scope){
-        $scope.messages = [
-          {sender: 'Alice', body:'Hello, how are you?', side:'left'},
-          {sender: 'Bob', body:'I am fine, how are you?', side: 'right'},
-          {sender: 'Alice', body:'Never been better', side: 'left'}
-        ];
+    
+    //Array of all messages
+    $scope.messages = [
+      {sender: 'Alice', body:'Hello, how are you?', side:'left'},
+      {sender: 'Bob', body:'I am fine, how are you?', side: 'right'},
+      {sender: 'Alice', body:'Never been better', side: 'left'}
+    ];
+    
+    //Function to add new messages
+    $scope.add_new_message = function(){
+        //Add new message to list of existing messages
+        $scope.messages.push({
+                sender: 'Bob',
+                body: $scope.message_body,
+                side:'right'
+            });
+        //Clear existing message
+        $scope.new_message = null;
+    }
 });
 
 
