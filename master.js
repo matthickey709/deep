8be7baef84b71c0351958bab8b91012613286f00
spandanb <spandan.777@gmail.com>
2015-07-21 23:41:43 -0400

    Added relay server, which is derived from server in server.js Also, Added master server. Changes to be committed: 	new file:   master.js 	modified:   package.json 	new file:   relay.js

---
+++ master.js
@@ -0,0 +1,24 @@
+//Master Server
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+var port = process.env.PORT || 8000;
+
+server.listen(port, function (){
+    console.log("MASTER server listening on port %d", port);
+});
+
+io.on('connection', function(socket){
+      socket.on('add user', function(username){
+          console.log("In add user: " + username);
+          socket.username = username;
+          socket.broadcast.emit('add user',
+            {username: username});
+      });
+      socket.on('new message', function(data){
+          console.log("New message: " data.username + " sent " + data.body);
+          socket.broadcast.emit('new message', data);
+      });
+
+
+      socket.on('disconnect', function(){});
+});
---
+++ relay.js
@@ -0,0 +1,72 @@
+/******** Constants ************/
+var MASTER_URL= MASTER_IP ? "http://" + MASTER_IP + ":8000" : "http://localhost:8000";
+
+/******** Dependencies ************/
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var methodOverride = require('method-override');
+var http = require('http');
+var socketIo = require('socket.io');
+var socketIoClient = require('socket.io-client');
+
+/******** Configuration  **********/
+var app = express();
+
+app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var server = http.createServer(app);
+var io = socketIo(server);
+
+var port = process.env.PORT || 3000;
+
+var master = socketIoClient(MASTER_URL);
+
+/******** Run Server **********/
+
+server.listen(port, function () {
+      
+      var host = server.address().address;
+      
+      console.log('Example app listening at http://%s:%s', host, port);
+});
+
+
+//Connection with Client
+io.on('connection', function (socket) {
+
+    // when the client emits 'new message', this listens and executes
+    socket.on('new message', function (data) {
+        //Relay the new message to the master
+        master.emit('new message', {
+                username: socket.username,
+                body: data
+            });
+    });
+
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', function (username) {
+        // we store the username in the socket session for this client
+        socket.username = username;
+        //Relay the add server to the master
+        master.emit('add user', username);
+    });
+});
+
+//Connection with Master
+master.on('add user', function(data){
+    console.log("Received 'add user' from master: " + data.username);
+    //broadcast 'add user' event to all clients 
+    io.sockets.emit('add user', data);
+});
+
+master.on('new message', function(data){
+    console.log("Received 'new message' from master: " + data.username + ": " + data.body);
+    //broadcast 'new message' event to all clients 
+    io.sockets.emit('new message', data);
+});
